8cfdb1c87170b64fa384ec4185969e3adffb17b7
Mark Nagy Georgy <1900520@eng.asu.edu.eg>
2023-05-04 13:01:20 +0300

    Add files via upload

---
+++ src/components/Navbar.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Button, Container,Nav, Navbar as NavbarBs } from 'react-bootstrap';
+
+import { NavLink } from 'react-router-dom';
+import '../App.css';
+import { useShoppingCart } from '../context/ShoppingCartContext';
+
+
+const Navbar = () => {
+  const{openCart,cartQuantity}=useShoppingCart();
+
+  return (<NavbarBs sticky="top" className="bg-pistachio shadow-sm mb-3"> 
+    <Container>
+        <Nav className='me-auto'>
+            <Nav.Link to= "/" as={NavLink}>
+                Home
+                </Nav.Link>
+            <Nav.Link to= "/about" as={NavLink} >
+                About
+                </Nav.Link>
+            <Nav.Link to= "/store" as={NavLink}>
+                Store
+            </Nav.Link>
+            
+        </Nav>
+        <Button
+            style={{ width: "3rem", height: "3rem", position: "relative" }}
+            variant="outline-primary"
+            className="rounded-circle"
+            onClick={openCart}
+          >
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart">
+            <circle cx="9" cy="21" r="1">
+                </circle>
+                <circle cx="20" cy="21" r="1">
+                    </circle>
+                    <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6">
+                        </path>
+                        </svg>
+                        <div
+              className="rounded-circle bg-danger d-flex justify-content-center align-item-center"
+              style={{
+                color: "white",
+                position: "absolute",
+                width: "1.5rem",
+                height: "1.5rem",
+                bottom: 0,
+                right: 0,
+                transform: "translate(25%, 25%)",
+              }}
+            >
+            {cartQuantity}
+            </div>
+                        
+        </Button>
+    </Container>
+  </NavbarBs>);
+};
+
+export default Navbar
+
+
+// sticky to fix position of nav bar on top
+//me-auto to put cart button at end of nav bar
